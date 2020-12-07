26a2a7653e5d28fcdd715cc89e96bc8f3947ed58
sirget <37872290+sirget@users.noreply.github.com>
2020-12-07 22:27:42 +0700

    upload

--- Sync/lab01/Program.cs
+++ Sync/lab01/Program.cs
@@ -6,27 +6,44 @@ namespace lab01
     class Program
     {
         private static int x = 0;
-
+        static object _lock = new object();
         static void funcA()
         {
+            Console.WriteLine("FuncA");
             int xx = 0;
-            while (xx < 5000)
+            lock (_lock)
             {
-                Console.WriteLine("FuncA : Round : {0} x = {1}", xx, x);
-                x++;
-                xx++;
+                while (xx < 500)
+                {
+                    Console.WriteLine("FuncA : Round : {0} x = {1}", xx, x);
+                    x++;
+                    xx++;
+                }
+
             }
+            Console.WriteLine("FuncAL");
+
+
         }
+
 
         static void funcB()
         {
+            Console.WriteLine("FuncB");
             int xx = 0;
-            while (xx < 5000)
+            lock (_lock)
             {
-                Console.WriteLine("FuncB : Round : {0} x = {1}", xx, x);
-                x++;
-                xx++;
+                while (xx < 500)
+                {
+
+                    Console.WriteLine("FuncB : Round : {0} x = {1}", xx, x);
+                    x++;
+                    xx++;
+                }
             }
+            Console.WriteLine("FuncBL");
+
+
         }
         static void Main(string[] args)
         {
